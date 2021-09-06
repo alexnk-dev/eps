ed4de138063dc89c753ea3f98c4ff3adea6b18be
Alexander Nikolayev <alexnikolayev.dev@gmail.com>
2021-09-06 20:20:49 +0300

    feat: add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    nav.classList.add("nav--active");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    nav.classList.remove("nav--active");
+    menuOpen = false;
+  }
+});
